0530fd25368f4e9fd36bc7b79bf9f2fa15e9c824
Jim Jenkins <jim@lindenlab.com>
2011-04-15 10:45:17 -0700

    Initial work. Implemented all Amazon SES actions except for SendRawEmail.

---
+++ README.md
@@ -0,0 +1,80 @@
+# Amazon SES
+
+A simple Amazon SES wrapper, supports the following actions:
+
+* DeleteVerifiedEmailAddress
+* GetSendQuota
+* GetSendStatistics
+* ListVerifiedEmailAddresses
+* SendEmail
+* VerifyEmailAddress
+
+Does not currently support SendRawEmail.
+
+## Install
+
+<pre>
+  npm install amazon-ses
+</pre>
+
+Or from source:
+
+<pre>
+  git clone git://github.com/jjenkins/node-amazon-ses.git
+  cd amazon-ses
+  npm link .
+</pre>
+
+## Verify Source Email
+
+Verify the source email address with Amazon.
+
+<pre>
+  var AmazonSES = require('amazon-ses');
+  var ses = new AmazonSES('access-key-id', 'secret-access-key');
+  ses.verifyEmailAddress('foo@mailinator.com');
+<pre>
+
+You will receive a confirmation email - click the link in that email to finish the verification process.
+
+## Send Email
+
+<pre>
+  ses.send({
+      from: 'foo@mailinator.com'
+    , to: ['bar@mailinator.com', 'jim@mailinator.com']
+    , subject: 'Test subject'
+    , body: {
+          text: 'This is the text of the message.'
+        , html: 'This is the <b>html</b> body of the message.'
+    }
+  });
+</pre>
+
+## Get verified email addresses
+
+<pre>
+  ses.listVerifiedEmailAddresses(function(result) {
+    console.log(result);
+  });
+</pre>
+
+## Deleted a verified email address
+
+<pre>
+  ses.deleteVerifiedEmailAddress('foo@mailinator.com', function(result) {
+    console.log(result);
+  });
+</pre>
+
+## Get Quota and Stats
+
+<pre>
+  ses.getSendQuota(function(result) {
+    console.log(result);
+  });
+
+  ses.getSendStatistics(function(result) {
+    console.log(result);
+  });
+</pre>
---
+++ main.js
@@ -0,0 +1,214 @@
+var _ = require('underscore');
+var crypto = require('crypto');
+var qs = require('querystring');
+var request = require('request');
+var xml = require('xml2js/lib');
+
+var AmazonSES = (function() {
+
+  var init = function(key, secret) {
+    this.accessKeyId = key;
+    this.secretAccessKey = secret;
+  };
+
+  var hmac = function(key, str) {
+    var hash = crypto.createHmac('sha256', key);
+    return hash.update(str).digest('base64');
+  };
+
+  var getDestinationList = function(addresses, type) {
+    var list = {};
+    _.each(addresses, function(address, idx) {
+        var key = 'Destination.'+ type +'Addresses.member.' + (idx + 1);
+        list[key] = address;
+    });
+    return list;
+  };
+
+  var buildMessage = function(opts) {
+    var params = {
+        'Action': 'SendEmail'
+      , 'Source': opts.from
+      , 'Message.Body.Text.Data': opts.body.text
+      , 'Message.Body.Text.Charset': 'UTF-8'
+      , 'Message.Body.Html.Data': opts.body.html
+      , 'Message.Body.Html.Charset': 'UTF-8'
+      , 'Message.Subject.Data' : opts.subject
+    };
+    _.extend(params, getDestinationList(opts.to, 'To'));
+    _.extend(params, getDestinationList(opts.cc, 'Cc'));
+    _.extend(params, getDestinationList(opts.bcc, 'Bcc'));
+
+    return params;
+  };
+
+  var call = function(opts) {
+    var host = 'email.us-east-1.amazonaws.com';
+    var path = '/';
+
+    var now = (new Date()).toUTCString();
+    var body = qs.stringify(opts.query);
+
+    var authorization =
+        'AWS3-HTTPS '
+      + 'AWSAccessKeyId=' + this.accessKeyId
+      + ',Algorithm=HmacSHA256'
+      + ',Signature=' + hmac(this.secretAccessKey, now)
+
+    var headers = {
+        'Date': now
+      , 'Host': host
+      , 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+      , 'Content-Length': body.length
+      , 'X-Amzn-Authorization': authorization
+    };
+
+    var options = {
+        method: 'POST'
+      , uri: 'https://' + host + path
+      , headers: headers
+      , body: body
+    };
+
+    request(options, function(error, response, body) {
+      var parser = new xml.Parser();
+      parser.addListener('end', function(data) {
+        var err = null;
+        if (data.hasOwnProperty('Error')) {
+          err = new Error(data.Error.Message);
+        }
+        opts.callback(err, data);
+
+      });
+      parser.parseString(body);
+    });
+  };
+
+  var Constructor = function(accessKeyId, secretAccessKey) {
+    init(accessKeyId, secretAccessKey);
+  };
+
+  Constructor.prototype = {
+    constructor: AmazonSES,
+    /**
+     * Verify an email address. This action causes a confirmation email
+     * message to be sent to the specified address.
+     *
+     * @params {String} email The email address to be verified
+     * @params {Function} callback
+     * @throws {Error}
+     */
+    verifyEmailAddress: function(email, callback) {
+      call({
+          query: {'Action': 'VerifyEmailAddress', 'EmailAddress': email}
+        , callback: function(err, data) {
+            if (err) throw err;
+            if (callback) {
+              callback(data.ResponseMetadata);
+            }
+        }
+      });
+    },
+    /**
+     * Return a list containing all of the email addresses that have been
+     * verified.
+     *
+     * @params {Function} callback
+     * @throws {Error}
+     */
+    listVerifiedEmailAddresses: function(callback) {
+      call({
+          query: {'Action': 'ListVerifiedEmailAddresses'}
+        , callback: function(err, data) {
+            if (err) throw err;
+
+            var result = data.ListVerifiedEmailAddressesResult;
+            if (callback) {
+              callback(result.VerifiedEmailAddresses.member);
+            } else {
+              throw Error("Must pass listVerifiedEmailAddresses a callback function");
+            }
+        }
+      });
+    },
+    /**
+     * Delete the specified email address from the list of verified
+     * addresses.
+     *
+     * @params {String} email The email address to delete
+     * @params {Function} callback
+     */
+    deleteVerifiedEmailAddress: function(email, callback) {
+      call({
+          query: {'Action': 'DeleteVerifiedEmailAddress', 'EmailAddress': email}
+        , callback: function(err, data) {
+            if (err) throw err;
+
+            if (callback) {
+              callback(data.ResponseMetadata);
+            }
+        }
+      });
+    },
+    /**
+     * Return the user's current sending limits.
+     *
+     * @params {Function} callback
+     */
+    getSendQuota: function(callback) {
+      call({
+          query: {'Action': 'GetSendQuota'}
+        , callback: function(err, data) {
+            if (err) throw err;
+
+            if (callback) {
+              callback(data.GetSendQuotaResult);
+            } else {
+              throw Error("Must pass getSendQuota a callback function");
+            }
+        }
+      });
+    },
+    /**
+     * Return the user's sending statistics. The result is a list of data
+     * points, representing the last two weeks of sending activity. Each
+     * data point in the list contains statistics for a 15-minute interval.
+     *
+     * @params {Function} callback
+     */
+    getSendStatistics: function(callback) {
+      call({
+          query: {'Action': 'GetSendStatistics'}
+        , callback: function(err, data) {
+            if (err) throw err;
+
+            if (callback) {
+              callback(data.GetSendStatisticsResult);
+            } else {
+              throw Error("Must pass getSendStatistics a callback function");
+            }
+        }
+      });
+    },
+    /**
+     * Composes an email message based on input data, and then immediately
+     * queues the message for sending.
+     *
+     * @params {Object} message The email message components
+     * @params {Function} callback
+     */
+    send: function(message, callback) {
+      call({
+          query: buildMessage(message)
+        , callback: function(err, data) {
+            if (err) throw err;
+            if (callback) callback(data);
+        }
+      });
+    }
+  };
+
+  return Constructor;
+
+}());
+module.exports = AmazonSES;
